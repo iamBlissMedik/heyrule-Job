3937695db8a4d2eab27267886653318c713ebf95
Ellams Bliss <ellamsbliss@gmail.com>
2023-06-07 08:02:18 +1000

    hey

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type Job from "./types/job";
+import JobListVue from "./components/JobList.vue";
+import type OrderTerm from "@/types/OrderTerm";
+
+const jobs = ref<Job[]>([
+  { title: "farm worker", location: "lon lon ranch", salary: 30000, id: "1" },
+  { title: "quarryman", location: "death mountain", salary: 40000, id: "2" },
+  { title: "flute player", location: "the lost woods", salary: 35000, id: "3" },
+  { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+  { title: "prison guard", location: "gerudo valley", salary: 32000, id: "5" },
+]);
+const order = ref<OrderTerm>("title");
+const handleClick = (term: OrderTerm) => {
+  order.value = term;
+};
+</script>
+
+<template>
+  <div class="app">
+    <header>
+      <div class="title">
+        <h1>HYRULE JOBS</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+    <JobListVue :jobs="jobs" :order="order" />
+  </div>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { computed, type PropType } from "vue";
+import type Job from "@/types/job";
+import type OrderTerm from "@/types/OrderTerm";
+const props = defineProps({
+  jobs: {
+    required: true,
+    type: Array as PropType<Job[]>,
+  },
+  order: {
+    required: true,
+    type: String as PropType<OrderTerm>,
+  },
+});
+
+const orderedJobs = computed(() => {
+  return [...props.jobs].sort((a: Job, b: Job) => {
+    return a[props.order] > b[props.order] ? 1 : -1;
+  });
+});
+</script>
+<template>
+  <div class="job-list">
+
+    <ul>
+      <li v-for="job in orderedJobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem omnis
+            voluptatum eius doloremque optio iusto sequi dignissimos. Pariatur
+            earum assumenda dolores possimus quidem quam, reprehenderit aliquid
+            consequuntur amet non facere.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+</style>
